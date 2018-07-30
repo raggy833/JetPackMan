0f2afbacd896f587960ad219a8b3e8a7e07c5659
Ragnar <raggym833@gmail.com>
2018-07-30 10:06:32 -0700

    Random Spawn Location
    Added a random spawn location with Gizmo.

--- Scripts/Spawner.cs
+++ Scripts/Spawner.cs
@@ -8,33 +8,43 @@ public class Spawner : MonoBehaviour {
     public float startTimeBtwSpawn;
     public float decreaseTime;
     public float minTime = 0.65f;
+	
+	public float xSize;
+	public float ySize;
 
     private float timeBtwSpawn;
     
 
 	// Use this for initialization
 	void Start () {
-		
+		timeBtwSpawn = startTimeBtwSpawn;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		if(timeBtwSpawn <= 0) {
-            Instantiate(wall, transform.position, Quaternion.identity);
-            timeBtwSpawn = startTimeBtwSpawn;
-            if (startTimeBtwSpawn > minTime) {
-                startTimeBtwSpawn -= decreaseTime;
-            }
+	if(GameManager.levelStarted){
+			if(timeBtwSpawn <= 0) {
+				Spawn();
+				timeBtwSpawn = startTimeBtwSpawn;
+				if (startTimeBtwSpawn > minTime) {
+					startTimeBtwSpawn -= decreaseTime;
+				}
 
-        } else {
-            timeBtwSpawn -= Time.deltaTime;
-        }
+			} else {
+				timeBtwSpawn -= Time.deltaTime;
+			}
+		}
+	}
+	
+	void OnDrawGizmos(){
+		Gizmos.color = Color.green;
+		Gizmos.DrawCube(transform.position, new Vector(xSize, ySize, 1);
 	}
 
-    //void Spawn() {
-    //    int spawnPointX = Random.Range(-450, 450);
-    //    int spawnPointY = Random.Range(-360, 350);
-    //    Vector3 spawnPosition = new Vector3(spawnPointX, spawnPointY, 0);
-    //    Instantiate(enemy, spawnPosition, Quaternion.identity);
-    //}
+    void Spawn() {
+        int spawnPointX = Random.Range(-1, 1);
+        int spawnPointY = Random.Range(-100, 100);
+        Vector3 spawnPosition = new Vector3(spawnPointX, spawnPointY, 0);
+        Instantiate(wall, spawnPosition, Quaternion.identity);
+    }
 }
